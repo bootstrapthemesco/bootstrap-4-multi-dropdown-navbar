e911bc6318ac96978591dbc1a5da9464b82c8b53
Ataur R <starxpeed@gmail.com>
2017-11-06 22:02:10 +0600

    edit js

--- js/bootstrap-4-navbar.js
+++ js/bootstrap-4-navbar.js
@@ -1,6 +1,9 @@
 
-//Copy this JS
-
+/*!
+ * Bootstrap 4 multi dropdown navbar ( https://bootstrapthemes.co/demo/resource/bootstrap-4-multi-dropdown-navbar/ )
+ * Copyright 2017.
+ * Licensed under the MIT license
+ */
 $( document ).ready( function () {
     $( '.navbar a.dropdown-toggle' ).on( 'click', function ( e ) {
         var $el = $( this );
