620ff52291ce46d991cad1ea512e1e35aa2a90fb
Ataur R <starxpeed@gmail.com>
2017-11-06 22:04:47 +0600

    GPL

--- js/bootstrap-4-navbar.js
+++ js/bootstrap-4-navbar.js
@@ -2,7 +2,7 @@
 /*!
  * Bootstrap 4 multi dropdown navbar ( https://bootstrapthemes.co/demo/resource/bootstrap-4-multi-dropdown-navbar/ )
  * Copyright 2017.
- * Licensed under the MIT license
+ * Licensed under the GPL license
  */
 $( document ).ready( function () {
     $( '.navbar a.dropdown-toggle' ).on( 'click', function ( e ) {
